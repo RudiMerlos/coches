10f3e232c5dbd2454e14888fa4a1507ff14b46e1
Rudi Merlos Carrión <rudimerlos@gmail.com>
2023-02-21 19:42:26 +0100

    Cositas

--- src/main/java/org/rmc/coches/service/CocheServiceImpl.java
+++ src/main/java/org/rmc/coches/service/CocheServiceImpl.java
@@ -5,16 +5,19 @@ import java.util.Optional;
 import org.rmc.coches.model.entity.Coche;
 import org.rmc.coches.model.repository.CocheRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CocheServiceImpl extends CommonServiceImpl<Coche, CocheRepository> implements CocheService {
 
     @Override
+    @Transactional(readOnly = true)
     public List<Coche> findAllCoches() {
         return this.repository.findAllCoches();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Coche> findCocheById(Long id) {
         return this.repository.findCocheById(id);
     }
--- src/main/java/org/rmc/coches/service/MarcaServiceImpl.java
+++ src/main/java/org/rmc/coches/service/MarcaServiceImpl.java
@@ -5,16 +5,19 @@ import java.util.Optional;
 import org.rmc.coches.model.entity.Marca;
 import org.rmc.coches.model.repository.MarcaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MarcaServiceImpl extends CommonServiceImpl<Marca, MarcaRepository> implements MarcaService {
 
     @Override
+    @Transactional(readOnly = true)
     public List<Marca> findAllMarcas() {
         return this.repository.findAllMarcas();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Marca> findMarcaById(Long id) {
         return this.repository.findMarcaById(id);
     }
